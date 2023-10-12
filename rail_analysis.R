56ec2479c208c5c667129aa93a73f0fa1b722fd5
githubUserName <akapoor@nd.edu>
2023-10-12 11:31:15 -0400

    updated

--- rail_analysis.R
+++ rail_analysis.R
@@ -2,7 +2,7 @@ library(dplyr)
 #data loading
 rail <-
   read.csv("~/Downloads/Rail_Equipment_Accident_Incident_Data.csv")
-View(rail)
+
 x <- colnames(rail)
 sort(x, decreasing = FALSE)
 missing_values <- sapply(rail, function(x)
@@ -26,38 +26,26 @@ rail_cleaned_data <-
     "Accident.Cause",
     "Contributing.Accident.Cause"
   )]
-View(rail_cleaned_data)
+#View(rail_cleaned_data)
 
-# Assuming "rail_cleaned_data" is your data frame with the "Accident.Cause" column
-# Split the text into words
+
 words <- unlist(strsplit(rail_cleaned_data$Accident.Cause, " "))
 
-# Create a table of word frequencies
 word_freq <- table(words)
 
-# Find the maximum frequency
 max_freq <- max(word_freq)
 
-# Find the words with maximum frequency
 words_with_max_freq <- names(word_freq)
 sort(words_with_max_freq, decreasing = TRUE)
 
-# Print the results
-cat("Words with maximum frequency:",
-    paste(words_with_max_freq, collapse = ", "),
-    "\n")
-cat("Maximum frequency:", max_freq, "\n")
-
 words <- unlist(strsplit(rail_cleaned_data$Accident.Cause, " "))
 
-# Create a table of word frequencies
+
 word_freq <- table(words)
 
-# Create a data frame with words and frequencies, sorted by frequency
 word_freq_df <-
   data.frame(Word = names(word_freq), Frequency = as.numeric(word_freq))
 
-# Sort the data frame by frequency in descending order
 word_freq_df <- word_freq_df[order(-word_freq_df$Frequency),]
 
 your_word_df <- "[Dd]efective"
